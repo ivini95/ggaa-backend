f8d9f5ffd4db41dbf949cc4eaec487a0f5d85522
ivini95 <viniximenesdev@gmail.com>
2026-08-05 18:31:19 -0300

    fix(middlewares): adiciona excecao para rotas de inicializacao sem body

--- src/middlewares/validateBody.middleware.ts
+++ src/middlewares/validateBody.middleware.ts
@@ -1,7 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const validateBodyMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  // Apenas metodos que esperam um corpo na requisicao
+  // Exceção: Se for a rota de inicializar entregas, ignora a checagem de body obrigatório
+  if (req.path.includes('/inicializar/atividade/')) {
+    return next();
+  }
+
+  // Apenas métodos que esperam um corpo na requisição
   if (['POST', 'PUT', 'PATCH'].includes(req.method)) {
     if (!req.body || Object.keys(req.body).length === 0) {
       return res.status(400).json({
